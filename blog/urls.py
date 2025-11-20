975e5775be6caf0739701f1cb828b889dad81cea
Lê Minh Trí <69792610+letri175@users.noreply.github.com>
2025-11-20 23:19:17 +0700

    Delete blog directory

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="post-page"),
-    path("post/<slug:slug>", views.post_detail, name="post-detail-page")
-]
